a4caaafb9e3c4e8fa086c33819063b7b434bb5c3
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2012-05-18 18:25:08 +0200

    adding ability and copycopter file

---
+++ app/models/ability.rb
@@ -0,0 +1,35 @@
+class Ability
+  include CanCan::Ability
+ 
+  def initialize(user)
+    return unless user
+    @user = user
+
+    @user.roles.each do |role|
+      self.send("#{role.name.underscore}_abilities")
+    end
+  end
+
+  def admin_abilities
+  end
+
+  def uploader_abilities
+  end
+
+  def entered_apprentice_abilities  
+  end
+
+  def fellow_craft_abilities
+  end
+
+  def master_mason_abilities
+  end
+
+  def worshipful_master_abilities
+  end
+
+  def member_of_council_abilities
+  end
+end
+
+
---
+++ config/initializers/copycopter.rb
@@ -0,0 +1,6 @@
+#CopycopterClient.configure do |config|
+#  config.api_key = '4d28ddc1f06e995243bdc5ba958a90d6'
+#  config.secure = false
+#  config.host = 'copycopter.deftwork.com'
+#  config.ca_file = Rails.root.join("config", "ca.pem").to_s
+#end
