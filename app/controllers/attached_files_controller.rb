dd6a25f6dabed5ff83681622cdb77a782dc79ba8
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2012-05-19 18:48:23 +0200

    fixes #970 @0h15m

--- app/controllers/attached_files_controller.rb
+++ app/controllers/attached_files_controller.rb
@@ -4,10 +4,11 @@ class AttachedFilesController < ApplicationController
   load_and_authorize_resource :except => :create
   
   def download
-    fd = FileDownload.ne
+    fd = FileDownload.new
     fd.user = current_user
     fd.attached_file = @attached_file
     fd.remote_ip = current_user.current_sign_in_ip
+    fd.save!
     send_data @attached_file.content, :filename => @attached_file.filename, :type => @attached_file.content_type
   end
   
