f79ecf0d0e3da0a2ebf449146e5c6479c9417ce0
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2012-05-19 12:59:20 +0200

    added categories and copycopter stuff

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,9 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name, :description
-
   extend FriendlyId
   friendly_id :name
+
+  attr_accessible :name, :description
+
 
   validates_presence_of :name
   validates_uniqueness_of :name
