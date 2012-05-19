4644cbc3fe25e0182a3ab4fe1dd69e6e602682bc
Max Trocha <matroc@tzi.de>
2012-05-19 15:15:09 +0200

    basic file attaching

--- app/controllers/attached_files_controller.rb
+++ app/controllers/attached_files_controller.rb
@@ -1,9 +1,8 @@
 class AttachedFilesController < ApplicationController
-  load_and_authorize_resource
-
-  def index
-  end
-
+  before_filter :authenticate_user!
+  check_authorization :except => :create
+  load_and_authorize_resource :except => :create
+  
   def show
   end
 
@@ -11,6 +10,17 @@ class AttachedFilesController < ApplicationController
   end
 
   def create
+    unless current_user.role_ids.include?(Role.find_by_name("Uploader").id)
+      redirect_to root_url, :alert => t("devise.error.access_denied")
+    end
+    @attached_file = AttachedFile.new do |af|
+      file = params[:attached_file].delete(:file)
+      af.filename = file.original_filename
+      af.content_type = file.content_type
+      af.content = file.tempfile.read
+      file.tempfile.delete
+      af.uploader_id = current_user.id
+    end
     if @attached_file.save
       redirect_to @attached_file, notice: t("activerecord.create_success", model: t("activerecord.models.attached_file"))
     else
