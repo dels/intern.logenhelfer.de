7a5a7c7ea1971b11b2a50d2a6eae80d85e220cc0
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2012-05-28 17:44:42 +0200

    added comments to ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -24,10 +24,12 @@ class Ability
     end
   end
 
+  # korrespondierender Schriftfuehrer
   def secretary_abilities
     can [:index, :show, :edit, :update], User
   end
 
+  # Deft
   def admin_abilities
     can :manage, Event
     can :manage, Category
@@ -37,24 +39,30 @@ class Ability
     can :index, FileDownload
   end
 
+  # 
   def uploader_abilities
     can :manage, Category
     can :manage, Directory
     can :manage, AttachedFile
   end
 
+  # Lehrling
   def entered_apprentice_abilities
   end
 
+  # Geselle
   def fellow_craft_abilities
   end
 
+  # Meister
   def master_mason_abilities
   end
-
+  
+  # Meister vom Stuhl
   def worshipful_master_abilities
   end
 
+  # Mitglied des Beamtenrates
   def member_of_council_abilities
   end
 end
