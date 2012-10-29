9a3bbbdfb62c1b30b2881569dcbaaebf1df525df
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2012-10-29 11:49:40 +0100

    replaced secretary email by korr.schriftfuehrer@fwze.de

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,13 +7,7 @@ class UserMailer < ActionMailer::Base
   #   en.user_mailer.change_notification.subject
   #
   def change_notification(changed_user)
-    @secretary = User.get_secretary
-    unless @secretary
-      logger.error "no secretary defined."
-      return
-    end
-    logger.error "sending mail to #{@secretary.email}"
     @changed_user = changed_user
-    mail to: @secretary.email, subject: I18n.t('user_mailer.change_notification.subject')
+    mail to: "korr.schriftfuehrer@fwze.de", subject: I18n.t('user_mailer.change_notification.subject')
   end
 end
