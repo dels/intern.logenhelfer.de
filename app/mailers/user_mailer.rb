2ff749ace9790f3b2cb07c4c250d12db59c1602e
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2012-11-07 13:36:31 +0100

    fixed usermail (should)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,7 @@ class UserMailer < ActionMailer::Base
   #
   def change_notification(changed_user)
     @changed_user = changed_user
+    @secretary = UserRole.where(:role_id => Role.where(:name => 'Secretary').first).first.user
     mail to: "korr.schriftfuehrer@fwze.de", subject: I18n.t('user_mailer.change_notification.subject')
   end
 end
