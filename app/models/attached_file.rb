8d642be083ad4ab78ada6145349b2063446a1788
Dominik Menke <dmke@tzi.de>
2012-11-13 20:50:04 +0100

    filename mangeling when uploading files with existing names - fixes #1138 @0h40m

--- app/models/attached_file.rb
+++ app/models/attached_file.rb
@@ -4,6 +4,7 @@ class AttachedFile < ActiveRecord::Base
 
   extend FriendlyId
   friendly_id :uuid
+  before_create :slug_name
 
   belongs_to :directory
   belongs_to :uploader, :class_name => "User"
@@ -14,22 +15,21 @@ class AttachedFile < ActiveRecord::Base
   attr_accessible :filename, :content, :content_type, :directory_id, :role_ids
 
   default_scope where(:deleted => (Rails.env.archive? || Rails.env.archive_dev?))
-  
-  
+
+
   def size
     if 0 > self.content_length
       reload
-      self.content_length = self.content.length 
+      self.content_length = self.content.length
       self.save
     end
     content_length
   end
-  
+
   def path_array
     [directory.category, directory, self]
   end
-  
-    
+
   def delete
     if APP_CONFIG[:archive]
       self.deleted = false
@@ -38,5 +38,16 @@ class AttachedFile < ActiveRecord::Base
       self.deleted = true
     end
     self.save
+  end
+
+  def slug_name
+    o_name = self.filename
+    if AttachedFile.exists?(filename: o_name)
+      i = 2
+      begin
+        self.filename = o_name.gsub(/^(.*?)\.(.*?)$/, "\\1 (#{i}).\\2")
+        i += 1
+      end while AttachedFile.exists?(filename: self.filename)
+    end
   end
 end
