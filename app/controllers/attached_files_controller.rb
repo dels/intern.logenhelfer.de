cfcb6cb66a91a96411137d15911a27f513d37dba
Dominik Menke <dmke@tzi.de>
2012-11-13 20:08:26 +0100

    capture empty file uploads - fixes #1146 @1h10m

--- app/controllers/attached_files_controller.rb
+++ app/controllers/attached_files_controller.rb
@@ -3,7 +3,7 @@ class AttachedFilesController < ApplicationController
   check_authorization :except => :create
   load_and_authorize_resource :except => :create
   helper_method :sort_column, :sort_direction
-  
+
   def download
     fd = FileDownload.new
     fd.user = current_user
@@ -12,7 +12,7 @@ class AttachedFilesController < ApplicationController
     fd.save!
     send_data @attached_file.content, :filename => @attached_file.filename, :type => @attached_file.content_type
   end
-  
+
   def show
   end
 
@@ -25,20 +25,29 @@ class AttachedFilesController < ApplicationController
     unless can?(:create, AttachedFile)
       redirect_to root_url, :alert => t("devise.error.access_denied")
     end
-    @attached_file = AttachedFile.new do |af|
-      file = params[:attached_file].delete(:file)
-      af.filename = file.original_filename
-      af.content_type = file.content_type
-      af.content = file.tempfile.read
-      file.tempfile.delete
-      af.uploader_id = current_user.id
-      af.directory_id = Directory.find(params[:directory_id]).id
-      af.role_ids = params[:attached_file][:role_ids]
-    end
-    @attached_file.content_length = @attached_file.content.length
-    if @attached_file.save
-      redirect_to [@attached_file.directory.category, @attached_file.directory], notice: t("activerecord.create_success", model: t("activerecord.models.attached_file"))
+    file = params[:attached_file].delete(:file)
+    if file.present?
+      @attached_file = AttachedFile.new do |af|
+        af.filename     = file.original_filename
+        af.content_type = file.content_type
+        af.content      = file.tempfile.read
+        file.tempfile.delete
+        af.uploader_id  = current_user.id
+        af.directory_id = Directory.find(params[:directory_id]).id
+        af.role_ids     = params[:attached_file][:role_ids]
+      end
+      @attached_file.content_length = @attached_file.content.length
+      if @attached_file.save
+        redirect_to [@attached_file.directory.category, @attached_file.directory], notice: t("activerecord.create_success", model: t("activerecord.models.attached_file"))
+      else
+        render :new
+      end
     else
+      @attached_file = AttachedFile.new do |af|
+        af.directory = Directory.find(params[:directory_id])
+        af.role_ids = params[:attached_file][:role_ids]
+      end
+      flash.now[:error] = t('activerecord.upload_failure')
       render :new
     end
   end
