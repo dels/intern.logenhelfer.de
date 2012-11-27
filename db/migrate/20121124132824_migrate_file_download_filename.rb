3f4989d11535b43da12a54f99d9ff54d76b9f1b2
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2012-11-27 14:58:54 +0100

    fixes #1189 @0h5m

--- db/migrate/20121124132824_migrate_file_download_filename.rb
+++ db/migrate/20121124132824_migrate_file_download_filename.rb
@@ -1,6 +1,7 @@
 class MigrateFileDownloadFilename < ActiveRecord::Migration
   def up
     FileDownload.all.each do |fd|
+      next unless fd.attached_file
       fd.filename = fd.attached_file.filename
       fd.save!
     end
