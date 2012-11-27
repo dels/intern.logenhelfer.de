4522b75ada7e65e21da4da8c70bbfdc2af173b22
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2012-11-27 15:01:55 +0100

    missed one file...

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -32,6 +32,6 @@ class UserMailer < ActionMailer::Base
       [a, changes]
     end.compact
 
-    mail to: "korr.schriftfuehrer@fwze.de", subject: I18n.t('user_mailer.change_notification.subject')
+    mail to: APP_CONFIG[:user_change_notification_email], subject: I18n.t('user_mailer.change_notification.subject')
   end
 end
