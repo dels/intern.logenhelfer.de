5203f304c6f5f1a013a36a3856844cce39b1b763
Dominik Menke <dmke@tzi.de>
2012-12-03 14:52:39 +0100

    oops

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121130151736) do
+ActiveRecord::Schema.define(:version => 20121203132723) do
 
   create_table "academic_titles", :force => true do |t|
     t.string   "title"
