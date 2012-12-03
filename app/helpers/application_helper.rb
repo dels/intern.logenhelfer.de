c0068c149118702ced91aea04e4a7d40800b954a
Dominik Menke <dmke@tzi.de>
2012-12-03 19:30:41 +0100

    oops - fixes #1206

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,4 +44,9 @@ module ApplicationHelper
     end
   end
 
+  def limited_editing?
+    @le ||= current_user.role_ids & Role.where(name: ['Admin', 'Secretary']).pluck(:id)
+    @le.blank?
+  end
+
 end
--- app/helpers/user_helper.rb
+++
@@ -1,8 +0,0 @@
-module UserHelper
-
-  def limited_editing?
-    @le ||= current_user.role_ids & Role.where(name: ['Admin', 'Secretary']).pluck(:id)
-    @le.blank?
-  end
-
-end
