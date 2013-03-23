9187f74bd2c6f2d5b00a3f8a1a2559c4ecaa8ad9
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2013-03-23 12:45:51 +0100

    fixed user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,7 +22,8 @@ describe User do
 
   it "fails to assign the master mason status" do 
     master_without_fellow_craft = FactoryGirl.build(:user, :entered_apprentice_since => 5.years.ago, :master_mason_since => 3.years.ago)
-    master_without_fellow_craft.master_mason_since.should be_nil
+    master_without_fellow_craft.should_not be_valid
+#    master_without_fellow_craft.master_mason_since.should be_nil
   end
 
 end
