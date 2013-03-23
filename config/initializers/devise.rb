d66a4992628420e8f2b2d6a408a82dc539600be8
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2013-03-23 14:28:56 +0100

    added email regex validation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -119,7 +119,7 @@ Devise.setup do |config|
   # Email regex used to validate email formats. It simply asserts that
   # an one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  # config.email_regexp = /\A[^@]+@[^@]+\z/
+  config.email_regexp = /^[a-z0-9_\.\-]+@{1}[a-z0-9_\.\-]{2,}\.(me|com|me|uk.co|info|org|net|biz|edu|gov|mil|io|mobi|name|aero|asia|jobs|museum|eu|de|at|be|ch|dk|es|gr|im|it|li|lt|lu|nl|no|nu|pl|pro|pt|ro|ru|se|te|travel|ag|ca|co|us){1}$/i
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,8 @@ describe User do
   it { should validate_presence_of :lastname }
   it { should validate_presence_of :date_of_birth }
   it { should validate_presence_of :matriculation_number }
+  it { should validate_uniqueness_of :matriculation_number }
+  it { should validate_uniqueness_of :email }
 
   it "creates a valid entered apprentice" do 
     FactoryGirl.build(:user, :entered_apprentice_since => 5.years.ago).should be_valid
@@ -18,6 +20,32 @@ describe User do
 
   it "creates a valid master mason" do 
     FactoryGirl.build(:user, :entered_apprentice_since => 5.years.ago, :fellow_craft_since => 4.years.ago, :master_mason_since => 3.years.ago).should be_valid
+  end
+
+  it "requires an at sign" do
+    should_not allow_value("simple_string_without_at").for(:email)
+  end
+
+  it "reqjects a too short domain" do
+    should_not allow_value("user@d.com").for(:email)
+  end
+
+  it "requires a TLD" do 
+    should_not allow_value("user@domain").for(:email)
+  end
+
+  it "expects a TLD length of two char at minumum" do
+    should_not allow_value("user@domain.e").for(:email)
+  end
+
+  it "allows a valid email address" do 
+    should allow_value("user@domain.de").for(:email)
+  end
+
+  it "requires a password length of 8 at minimum" do
+    should_not allow_value("123456").for(:password)
+    should_not allow_value("1234567").for(:password)
+    should allow_value("12345678").for(:password)
   end
 
   it "fails to assign the master mason status" do 
