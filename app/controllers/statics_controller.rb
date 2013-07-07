0ca8879c502aff705cefa08fa17cce084d15f1eb
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2013-07-07 18:55:26 +0200

    limited news on first page to 5

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -4,19 +4,13 @@ class StaticsController < ApplicationController
   respond_to :html, :text
 
   def index
-    @announcements = Announcement.page(params[:page])
+    @announcements = Announcement.page(params[:page]).per(5)
   end
 
   def impressum
   end
 
-#  def overview
-#    @start_date, @end_date = get_safe_start_end_date(params[:start_date], params[:end_date])
-#    redirect_to tasks_path if current_user.acts_like? :FacilityManager
-#  end
-
   def robots_txt
-#    respond_to :text
     render 'robots_txt', :content_type => "text/plain", :layout => false
   end
 
