95294c571f7d56012ff9489d949abf5330d811aa
Dominik Elsbroek <de_git@elsbroek.com>
2015-01-30 09:27:50 +0100

    seekers email addresses are links only for whorshipful master

--- app/helpers/seekers_helper.rb
+++ app/helpers/seekers_helper.rb
@@ -3,7 +3,8 @@ module SeekersHelper
   def seekers_contact_data(seeker)
     case seeker.preferred_way_of_contact
     when 10
-      return mail_to(seeker.address.email, seeker.address.email, subject: I18n.t('mail.seeker.subject'), body: I18n.t('mail.seeker.body', seeker: seeker.lastname, whorshipful_master: User.worshipful_master.fullname))
+      return seeker.address.email unless current_user.worshipful_master?
+      return mail_to(seeker.address.email, seeker.address.email, subject: I18n.t('mail.seeker.subject'), body: I18n.t('mail.seeker.body', seeker: seeker.lastname, whorshipful_master: User.worshipful_master.fullname)) 
     when 20
       return seeker.address.phone
     when 30
