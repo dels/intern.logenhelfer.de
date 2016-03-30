68cb385d0e94e37548d085928c8e3ecacaeaccc7
Dominik Elsbroek <de_git@elsbroek.com>
2016-03-30 10:00:36 +0200

    just changed capital letters to non-capital ones

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     column = column.to_s
     title ||= column.titleize
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil
-    direction = (column == sort_column && sort_direction == "aSc") ? "deSc" : "aSc"
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
     link_to title, {:sort_by => column, :direction => direction}, {:class => css_class}
   end
 
