6d875f063c2e69fc4dd14c742f91aef714e9e556
Dominik Elsbroek <de_git@elsbroek.com>
2017-12-13 00:52:33 +0100

    improved meta info page

--- config/initializers/git_version.rb
+++ config/initializers/git_version.rb
@@ -1,9 +1,11 @@
 APP_VERSION = {
   :major => 0,
-  :minor => 4,
+  :minor => 9,
   :patch => `git log -n1 --date=short --pretty=format:"%h"`.chomp,
-  :last_commit_at => `git log -n1 --date=short --pretty=format:"%ad"`.chomp,
-  :deployed_at => Time.now.strftime("%d.%m.%Y")
+  :hash =>  `git log -n1 --date=short --pretty=format:"%H"`.chomp,
+  :last_commit_at => `git log -n1 --date=short --pretty=format:"%ct"`.chomp,
+  :deployed_at => Time.now.strftime("%d.%m.%Y"),
+  :deployed_at_full => Time.now
 }.freeze
 
 APP_VERSION_STRING = [APP_VERSION[:major], APP_VERSION[:minor], APP_VERSION[:patch]].join('.').freeze
