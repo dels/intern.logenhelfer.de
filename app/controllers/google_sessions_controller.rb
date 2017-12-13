8b1b17a5e2b8929cb2520f5cb1bb6b7cb32e2c63
Dominik Elsbroek <de_git@elsbroek.com>
2017-12-13 16:12:27 +0100

    fixed issue with google sessions

--- app/controllers/google_sessions_controller.rb
+++ app/controllers/google_sessions_controller.rb
@@ -1,7 +1,9 @@
 class GoogleSessionsController < AuthorizedController
+
+  skip_authorization_check
   
   def create
-    user = User.from_omniauth(current_user, env["omniauth.auth"])
+    user = User.from_omniauth(current_user, request.env["omniauth.auth"])
     session[:google_user_id] = user.id
     redirect_to google_sync_users_path
   end
